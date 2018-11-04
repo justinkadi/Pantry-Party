8e660fdb18be2a81056b49500c565dd37f30e645
AD s2dios <ad.s2dios@gmail.com>
2018-11-04 01:31:10 -0700

    Initial Commit

---
+++ Pantry Party/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pantry Party
+//
+//  Created by Adriel AD Tan on 11/4/18.
+//  Copyright © 2018 nowayste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
